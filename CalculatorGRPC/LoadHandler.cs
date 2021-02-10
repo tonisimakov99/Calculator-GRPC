f0ca6c52106c279bfc030c683b997ec3129c8031
anton simakov <toni.simakov.99@bk.ru>
2021-02-10 19:50:25 +0500

    first commit

---
+++ CalculatorGRPC/LoadHandler.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GrpcService1
+{
+    public class LoadHandler
+    {
+        private readonly RequestDelegate _next;
+        private readonly int maxCount;
+        private readonly ILogger<LoadHandler> logger;
+        private int count = 0;
+        public LoadHandler(RequestDelegate next, int maxCount, ILogger<LoadHandler> logger)
+        {
+            this._next = next;
+            this.maxCount = maxCount;
+            this.logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            logger.LogInformation($"total count = {count}");
+
+            Interlocked.Increment(ref count);
+            if (count < maxCount)
+            {
+                logger.LogInformation($"incremented count = {count}");
+                await _next(context);
+            }
+            else
+            {
+                await Task.Run(() => context.Response.StatusCode = 503);
+            }
+            Interlocked.Decrement(ref count);
+            logger.LogInformation($"decremented count = {count}");
+        }
+    }
+}
---
+++ CalculatorGRPC/Services/CalculatorService.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GrpcService1.Services
+{
+    public class CalculatorService : Calculator.CalculatorBase
+    {
+        private readonly ILogger<CalculatorService> _logger;
+        private readonly IDictionary<string, Func<double[], double>> funcs;
+
+        public CalculatorService(ILogger<CalculatorService> logger, IDictionary<string, Func<double[],double>> funcs)
+        {
+            _logger = logger;
+            this.funcs = funcs;
+        }
+
+        public override Task<MathResponse> Calculate(MathRequest request, ServerCallContext context)
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+            return Task.FromResult(new MathResponse() { Result = funcs[request.Func](request.Param.ToArray()) });
+        }
+    }
+}
